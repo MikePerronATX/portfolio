e831eba83e35fdaa11fd38de982dee5e64a2bb20
Michael Perron <mikeperron.atx@gmail.com>
2022-07-08 19:18:26 -0500

    Added Portfolio and Contact Form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
 import './contact.scss'
+import { MdOutlineEmail } from 'react-icons/md'
+import { RiMessengerLine } from 'react-icons/ri'
+import { BsWhatsapp } from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_3t0s51n', 'template_c9b9v8y', form.current, 'Ebim_LKu8Vj6uHu_9')
+
+    e.target.reset()
+  };
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='.contact__option-icon' />
+            <h4>Email</h4>
+            <h5>mikeperron.atx@gmail.com</h5>
+            <a href="mailto:mikeperron.atx@gmail.com" target='_blank'>Send a message</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className='.contact__option-icon' />
+            <h4>Messenger</h4>
+            <h5>mikeperron.atx@gmail.com</h5>
+            <a href="https://m.me/thewanderingcajun" target='_blank'>Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className='.contact__option-icon' />
+            <h4>WhatsApp</h4>
+            <h5>+123456789</h5>
+            <a href="https://api.whatsapp.com/send?phone" target='_blank'>Send a message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTION */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="text" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,21 +1,66 @@
 import React from 'react'
 import './portfolio.scss'
+import IMG1 from '../../assets/portfolio1.png'
+import IMG2 from '../../assets/portfolio2.png'
+import IMG3 from '../../assets/portfolio3.png'
+import IMG4 from '../../assets/portfolio4.png'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Imaginary Dashboard 1',
+    github: 'https://github.com/MikePerronATX',
+    demo: 'https://dribbble.com/Alien_pixels'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Imaginary Dashboard 2',
+    github: 'https://github.com/MikePerronATX',
+    demo: 'https://dribbble.com/Alien_pixels'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Imaginary Dashboard 3',
+    github: 'https://github.com/MikePerronATX',
+    demo: 'https://dribbble.com/Alien_pixels'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Imaginary Dashboard 4',
+    github: 'https://github.com/MikePerronATX',
+    demo: 'https://dribbble.com/Alien_pixels'
+  },
+]
 
 const Portfolio = () => {
   return (
     <section id='portfolio'>
       <h5>My Past Work</h5>
       <h2>Portfolio</h2>
+
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <h3>This is a portfolio item title</h3>
-            <a href="https://github.com/MikePerronATX" className='btn'>GitHub</a>
-            <a href="https://.com" className='btn btn-primary'>Live Demo</a>
-          </div>
-        </article>
-      </div>
-    </section>
+        {
+          data.map(({ id, image, title, github, demo }) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-image">
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn' target='_blank'>GitHub</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
+      </div >
+    </section >
   )
 }
 
